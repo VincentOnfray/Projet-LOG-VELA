b99e995a6dc61178a6823cb26eed81758d37021c
Lucas Guglielmi <lucas.guglielmi@viacesi.fr>
2023-01-09 17:10:35 +0100

    Signin front fini

--- app-utilisateur-final/src/views/SignView.vue
+++ app-utilisateur-final/src/views/SignView.vue
@@ -1,22 +1,18 @@
 <template>
-  <div class="LogIn">
-    <h3>Bienvenue sur l'écran de connexion de test</h3>
-    <LogMe></LogMe>
-  </div>
   <div class="SignIn">
-    <h3>Bienvenue sur l'écran de connexion de test</h3>
-    <LogMe></LogMe>
+    <h3>Veuillez remplir le formulaire</h3>
+    <SignIn></SignIn>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import LogMe from '@/components/LogMe.vue'; // @ is an alias to /src
+import SignIn from '@/components/SignIn.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    LogMe,
+    SignIn,
   },
 })
-export default class AuthentificationView extends Vue {}
+export default class SignView extends Vue {}
 </script>
