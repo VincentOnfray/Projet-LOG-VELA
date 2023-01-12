dcf4304ecb6b3e22e251ca0bc1b0211ff98b46ca
Lucas Guglielmi <lucas.guglielmi@viacesi.fr>
2023-01-12 12:19:05 +0100

    Update NavBar.vue
    Changement dans le design en cours

--- app-utilisateur-final/src/components/tools/NavBar.vue
+++ app-utilisateur-final/src/components/tools/NavBar.vue
@@ -1,111 +1,92 @@
 <template>  
-  <nav class="bar">
-    <img class="logo" src="../../ressources/CESIEATS.png"/>
+  <nav class="bar"> 
     <ol>
-      <li class="menu">
-        <button><img src="../../ressources/Menu.png"></button>
-        <!--<router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>-->
+      <li class="home">
+        <router-link to="/">
+          <img class="logo" src="../../ressources/CESIEATS.png"/>
+        </router-link>    
       </li>
+      <!--<div class="menu">
+        <button><img src="../../ressources/Menu.png"></button>
+      </div>-->
       <li class="pop">
         <router-link to="/" style="color:white">Les plus populaires</router-link>
-        <!--<a href="#">(les plus populaires)</a>-->
       </li>
       <li class="av">
         <router-link to="/" style="color:white">Les plus avantageux</router-link>
-        <!--<a href="#">(les plus avantageux)</a>-->
       </li>
       <li class="recherche">
         <input class="champ" type="text" placeholder="Chercher..."/>
-        <!--
-<input class="bouton" type="image" value=" " src="../ressources/Loupe.png">-->
-        <button><img class="loupe" src="../../ressources/Loupe.png"></button>
+        <button class="loupe"><img src="../../ressources/Loupe.png"></button>
       </li>
       <li class="user" v-if="connect === true">
         <button><img src="../../ressources/Utilisateur.jpg"></button>
       </li>
       <li class="con" v-else>
-        <button>Connexion</button>
+        <button @click="routeConnect">Connexion</button>
       </li>
-    </ol>
+    </ol>    
+    
   </nav>
 </template>
 
 <script lang="ts">
+import router from '@/router';
 import { Options, Vue } from 'vue-class-component';
+import { RouterLink } from 'vue-router';
 
 @Options({
   
 })
 export default class NavBar extends Vue {
-  connect!: boolean
+  connect!: boolean;
+  routeConnect: ((payload: MouseEvent) => void) | undefined;
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .bar {
-  display: inline-block;
-  margin: 0;
-  background-color: #04AA6D;
-  overflow: hidden;
-  border: 2px solid black;
-  color:white;
-  width:70em;
-  
-}
-.champ{
-  height:2em;
-  font-size: 1em;
-}
-.logo{
-  width:14em;
-  float:left;
-  margin-top: 0;
-}
-.loupe{
-  height:2em;
-}
-.bar ol {
-  list-style-type: none;
-  padding-left: 0;
-}
-.bar li {
-  text-align: center;
-  padding: 14px 16px;
-  text-decoration: none;
-  font-size: 17px;
-  color:white;
-}
-.menu {
-  margin: 0 0 0 0;
-}
-.menu input {
-  padding: 20 20 20 20;
-}
-.pop {
-  margin: 0;
-  color:white;
-}
-.av {
-  margin: 0;
-  color:white;
-}
-.recherche {
-  margin: 40px 0 0 0;
-  height:20px;
-}
-.recherche .loupe {
-  float:center;
-  height:30px;
-}
-.user {
-  margin: 40px 0 0 0;
-}
-.user input {
-  border-radius:  100% 100%;
+  display: grid;
+  background-color: #04AA6D;  
+  grid-template-columns: repeat(5, 1fr);
 }
 li {
   display: inline-block;
+  background-color: #04AA6D;  
+  margin: 0 50% 0 0;
+}
+.home {
+  grid-row: 1;
+}
+.logo{
+  width:10em;
+  float:left;
+  margin-top: 0;
+}
+.pop {
+  grid-row: 2;
+}
+.av{
+  grid-row: 3;
+}
+.recherche {
+  /*margin: 0 40px 0 40px;*/
+  grid-row: 4;
+}
+.recherche .loupe img{
+  height: 1.4em;
+  width: 1.4em;
+}
+.champ{
+  font-size: 1em;
+}
+.user {
+  grid-row: 5;
+  margin: 0 40px 0 0;
+}
+.con {
+  grid-row: 5;
+  margin: 0 40px 0 0;
 }
 </style>
