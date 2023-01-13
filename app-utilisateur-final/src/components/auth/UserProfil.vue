0d23cdedc2a29aad81de8b490ee225b943883161
Lucas Guglielmi <lucas.guglielmi@viacesi.fr>
2023-01-13 15:49:42 +0100

    Création composant consultation de l'user

---
+++ app-utilisateur-final/src/components/auth/UserProfil.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profil">
+    <img src="../../ressources/Utilisateur.jpg"/>
+    <div>Nom d'utilisateur : {{Name}}</div>
+    <div>Adresse email : {{Email}}</div>
+    <br>
+  </div>
+
+  </template>
+  <script lang="ts">
+  import { Options, Vue } from 'vue-class-component';
+
+  @Options({
+    props: {
+    }
+  })
+  export default class LogMe extends Vue {
+    Name: string = "TEST";
+    Email: string = "TEST";
+  }
+  </script>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped>
+  .profil{
+    border: 2px dotted black ;
+  }
+  img {
+    width: 10em;
+  }
+  </style>
---
+++ app-utilisateur-final/src/views/UserView.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav-bar></nav-bar>
+  <Body class="User">
+    <user-profil></user-profil>
+  </Body>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import UserProfil from '@/components/auth/UserProfil.vue'; // @ is an alias to /src
+import NavBar from '@/components//tools/NavBar.vue'; 
+
+@Options({
+  components: {
+    UserProfil,
+    NavBar
+  },
+})
+export default class SignView extends Vue {}
+</script>
