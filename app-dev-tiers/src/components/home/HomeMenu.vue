b5f0da0db8459a11e0e7e61a76de49dd818496b5
Lucas Guglielmi <lucas.guglielmi@viacesi.fr>
2023-01-17 17:07:50 +0100

    Début de l'app dev tiers

---
+++ app-dev-tiers/src/components/home/HomeMenu.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="menu">
+    <div class="newOne">
+      <div>Livraison en cours :</div>
+      <div>Restaurant : {{AdresseRestau}}</div>
+      <div>Livraison : {{AdresseDestination}}</div>
+    </div>
+    <div class="newTwo" v-for="item in valides">
+      <home-prop commande="{{item}}"></home-prop>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HomeProp from '@/components/home/HomeProp.vue';
+
+@Options({
+  components: {
+    HomeProp
+  },
+})
+
+export default class HomeMenu extends Vue {
+valides!: {
+  Nom: string;
+  Origine: string;
+  Destination:string;
+}
+  AdresseRestau!: string;
+  AdresseDestination!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.menu {
+  display: grid;
+  grid-template-rows: 20px, 1fr;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 20px;
+}
+.newOne {
+  grid-column: 1;
+}
+.newTwo {
+  grid-column: 2;
+}
+</style>
